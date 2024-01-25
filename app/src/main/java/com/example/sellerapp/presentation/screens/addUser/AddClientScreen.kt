355c75a0e106d12caecc8e8ee2fb9c0ec7c852d9
Maqsudov Abdulbosit <abdulbosit9730@gmail.com>
2024-01-26 03:00:34 +0600

    Ui Splash Va Ui Biroz Fix qilindi, Va Navigation Qo'shildi

--- app/src/main/java/com/example/sellerapp/presentation/screens/addUser/AddClientScreen.kt
+++ app/src/main/java/com/example/sellerapp/presentation/screens/addUser/AddClientScreen.kt
@@ -1,4 +1,34 @@
 package com.example.sellerapp.presentation.screens.addUser
 
-class AddClientScreen {
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.sellerapp.R
+import com.example.sellerapp.databinding.DialogAddClientBinding
+import com.example.sellerapp.databinding.ScreenSplashBinding
+
+class AddClientScreen() : Fragment(R.layout.dialog_add_client) {
+    private val binding by viewBinding(DialogAddClientBinding::bind)
+    lateinit var saveButtonListener: (String)-> Unit
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    private fun initView() {
+        binding.imageView.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        binding.btnSave.setOnClickListener{
+            //saveButtonListener.invoke(binding.editTextClientName.text.toString())
+            findNavController().navigateUp()
+        }
+    }
 }
--- app/src/main/java/com/example/sellerapp/presentation/screens/main/pages/firstPage/HomePage.kt
+++ app/src/main/java/com/example/sellerapp/presentation/screens/main/pages/firstPage/HomePage.kt
@@ -1,11 +1,31 @@
 package com.example.sellerapp.presentation.screens.main.pages.firstPage
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.sellerapp.R
 import com.example.sellerapp.databinding.PageFirstBinding
 import com.example.sellerapp.databinding.ScreenMainBinding
+import com.example.sellerapp.presentation.screens.addUser.AddClientScreen
 
 class HomePage : Fragment(R.layout.page_first) {
     private val binding by viewBinding(PageFirstBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+
+
+        binding.btnAdd.setOnClickListener {
+            AddClientScreen().saveButtonListener = {
+
+            }
+            findNavController().navigate(R.id.addClientScreen)
+        }
+    }
 }
