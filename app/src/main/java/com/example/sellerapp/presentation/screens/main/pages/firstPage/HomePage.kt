fe03e4f1a3508857b61c1443a0372b30df2c8e0b
Maqsudov Abdulbosit <abdulbosit9730@gmail.com>
2024-01-26 01:42:20 +0600

    Ui begin screen

--- app/src/main/java/com/example/sellerapp/presentation/screens/main/pages/firstPage/HomePage.kt
+++ app/src/main/java/com/example/sellerapp/presentation/screens/main/pages/firstPage/HomePage.kt
@@ -1,8 +1,11 @@
 package com.example.sellerapp.presentation.screens.main.pages.firstPage
 
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.sellerapp.R
+import com.example.sellerapp.databinding.PageFirstBinding
+import com.example.sellerapp.databinding.ScreenMainBinding
 
 class HomePage : Fragment(R.layout.page_first) {
-
+    private val binding by viewBinding(PageFirstBinding::bind)
 }
