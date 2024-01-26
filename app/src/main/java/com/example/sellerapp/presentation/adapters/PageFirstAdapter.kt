aae58b30b588c1563065930c9acdad543c8cf9d3
Maqsudov Abdulbosit <abdulbosit9730@gmail.com>
2024-01-26 12:19:30 +0600

    User shakllantirish to'liq bo'ldi

--- app/src/main/java/com/example/sellerapp/presentation/adapters/PageFirstAdapter.kt
+++ app/src/main/java/com/example/sellerapp/presentation/adapters/PageFirstAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.sellerapp.presentation.adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -7,6 +8,9 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.sellerapp.data.model.UserData
 import com.example.sellerapp.databinding.ItemAddClientBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Timer
 
 class PageFirstAdapter: ListAdapter<UserData, PageFirstAdapter.ClientViewHolder>(GroupDiffUtil) {
     private var selectStudentIDListener: ((UserData) -> Unit)?=null
@@ -23,9 +27,11 @@ class PageFirstAdapter: ListAdapter<UserData, PageFirstAdapter.ClientViewHolder>
     }
 
     inner class ClientViewHolder(private var binding: ItemAddClientBinding) : ViewHolder(binding.root) {
-        private val productName = binding.productName
+        private val firstname = binding.firstname
         private val date = binding.date
-        private val productPrice = binding.price
+        private val lastname = binding.lastname
+        private val number = binding.number
+        var time = System.currentTimeMillis()
 
         init {
             binding.root.setOnClickListener {
@@ -40,9 +46,10 @@ class PageFirstAdapter: ListAdapter<UserData, PageFirstAdapter.ClientViewHolder>
         }
 
         fun bind() {
-            productName.text = currentList[absoluteAdapterPosition].productName
-            productPrice.text = currentList[absoluteAdapterPosition].productPrice
-//            date.text = currentList[absoluteAdapterPosition].
+            firstname.text = currentList[absoluteAdapterPosition].firstName
+            lastname.text = currentList[absoluteAdapterPosition].secondName
+            number.text = currentList[absoluteAdapterPosition].phoneNumber
+            date.text = getDate(System.currentTimeMillis(), "dd/MM/yyyy")
         }
     }
 
@@ -52,6 +59,16 @@ class PageFirstAdapter: ListAdapter<UserData, PageFirstAdapter.ClientViewHolder>
 
     override fun onBindViewHolder(holder: ClientViewHolder, position: Int) {
         holder.bind()
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun getDate(milliSeconds: Long, dateFormat: String): String {
+        val formatter = SimpleDateFormat(dateFormat)
+
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = milliSeconds
+        }
+        return formatter.format(calendar.time)
     }
 
 
