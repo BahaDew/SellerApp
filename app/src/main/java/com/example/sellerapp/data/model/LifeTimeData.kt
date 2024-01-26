f9ab3adb92d4abb32e85dff3b344fa1d98ffc911
boxodiryusubjonov7418 <boxodiryusubjonov7418@gmail.com>
2024-01-26 11:18:04 +0500

    ss

--- app/src/main/java/com/example/sellerapp/data/model/LifeTimeData.kt
+++ app/src/main/java/com/example/sellerapp/data/model/LifeTimeData.kt
@@ -1,10 +1,11 @@
 package com.example.sellerapp.data.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class LifeTimeData(
-    val id : Long,
+    @PrimaryKey(autoGenerate = true) val id : Long,
     val date : String,
     val summa : String,
     val payment : String,
